892d982ddffbea6c06f70b896a09569be0708ace
oladayo ahmod <oladayoahmod112@gmail.com>
2022-07-14 13:08:22 +0100

    setting deployment script

--- deploy/01-deploy.js
+++ deploy/01-deploy.js
@@ -2,4 +2,10 @@ module.exports = async ({getNamedAccounts,deployments})=>{
     const {deploy,logs} = deployments
     const {deployer} = await getNamedAccounts()
     const chainId = network.config.chainId
+
+    const Fundme = await deploy("FundMe",{
+        from : deployer,
+        args : [],
+        log : true
+    });
 }
