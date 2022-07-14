a692e866e97cf2494fa78894fa74552be37b83c1
oladayo ahmod <oladayoahmod112@gmail.com>
2022-07-14 15:47:24 +0100

    deploy mocks when no local network is detected

--- deploy/01-deploy.js
+++ deploy/01-deploy.js
@@ -1,13 +1,21 @@
- const {networkConfig} = require('../hardhat-helper-config')
+ const {networkConfig,develomentChains} = require('../hardhat-helper-config')
  const {network} = require("hardhat")
 module.exports = async ({getNamedAccounts,deployments})=>{
     const {deploy,log} = deployments
     const {deployer} = await getNamedAccounts()
     const chainId = network.config.chainId
 
+    let ethUsdPricefeedAddress
+    if (develomentChains.includes(network.name)) {
+        ethUsdPricefeedAddress = await deployments.get("MockV3Aggregator").address
+    }
+    else{
+        ethUsdPricefeedAddress = networkConfig[chainId]["ethUsdPricefeed"]
+    }
+
     const Fundme = await deploy("FundMe",{
         from : deployer,
-        args : [],
+        args : [ethUsdPricefeedAddress],
         log : true
     });
 }
