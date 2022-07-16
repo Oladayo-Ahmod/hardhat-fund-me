70248b0c11c8bd704a68345d0e05c34ae9e1d0b6
oladayo ahmod <oladayoahmod112@gmail.com>
2022-07-16 07:39:15 +0100

    testing smart contract deployment

--- test/unit/FundMeTest.js
+++ test/unit/FundMeTest.js
@@ -1,11 +1,17 @@
 const {deployments, ethers, getNamedAccounts} = require('hardhat')
+const {assert} = require('chai')
 
 describe("FundMe", ()=>{
 
-    let fundme, deployer
+    let fundme, deployer, mockV3Aggregator
     beforeEach(async ()=>{
         deployer = (await getNamedAccounts()).deployer
         await deployments.fixture(["all"])
         fundme = ethers.getContract("FundMe",deployer)
+        mockV3Aggregator = ethers.getContract('MockV3Aggregator',deployer)
+    })
+    it("it gets aggregator address correctly",async ()=>{
+        const response = await fundme.priceFeed()
+        assert.equal(response,mockV3Aggregator.address)
     })
 })
